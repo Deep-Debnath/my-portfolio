065c64cc15a9849ec28eb336a68d8387e8047357
Deepvai <deepdebnath7432@gmail.com>
2025-10-27 16:36:46 +0530

    readme

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import Buttons from "./buttons";
 import Footer from "./footer";
-import { ArrowForwardIos } from "@mui/icons-material";
 
 const parent = {
   hidden: { opacity: 0 },
@@ -18,32 +17,31 @@ const child = {
   show: { y: 0, opacity: 1, transition: { duration: 0.6 } },
 };
 
-export default function Hero({ open }) {
+export default function Hero() {
   return (
     <div className="hero relative sm:h-screen w-full overflow-hidden">
-      {!open && (
-        <motion.div
-          variants={parent}
-          initial="hidden"
-          animate="show"
-          className="absolute sm:top-[10%] top-[5%] left-[10%] text-cyan-100"
+      <motion.div
+        variants={parent}
+        initial="hidden"
+        animate="show"
+        className="absolute sm:top-[10%] top-[5%] left-[10%] text-cyan-100"
+      >
+        <motion.h1
+          variants={child}
+          className="text-3xl sm:text-4xl md:text-5xl font-bold"
         >
-          <motion.h1
-            variants={child}
-            className="text-3xl sm:text-4xl md:text-5xl font-bold"
-          >
-            DEEP DEBNATH
-          </motion.h1>
-          <motion.p
-            variants={child}
-            className="text-md pl-1 sm:text-lg mt-2 tracking-wide"
-          >
-            Turning ideas into interactive web interfaces
-          </motion.p>
-        </motion.div>
-      )}
-      {!open && <Buttons />}
-      {!open && <Footer />}
+          DEEP DEBNATH
+        </motion.h1>
+        <motion.p
+          variants={child}
+          className="text-md pl-1 sm:text-lg mt-2 tracking-wide"
+        >
+          Turning ideas into interactive web interfaces
+        </motion.p>
+      </motion.div>
+
+      <Buttons />
+      <Footer />
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,14 @@
 "use client";
 import Hero from "./components/hero";
-import { useEffect, useState } from "react";
-import Explore from "./components/intro";
+
 import Logo from "./components/logos";
 
 export default function Home() {
-  const [open, setopen] = useState(true);
-  useEffect(() => {
-    setopen(false);
-  }, [open]);
   return (
     <>
       <div>
-        <Hero open={open} />
-        <Logo open={open} />
-        {open && <Explore setopen={setopen} open={open} />}
+        <Hero />
+        <Logo />
       </div>
     </>
   );
